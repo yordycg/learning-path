2a3e753e8d3886be095d839e5324344121118b9a
yordycg <yordycarmona.dev@gmail.com>
2026-08-15 16:57:12 -0400

    feat(c): draft pseudocode for dynamic note recording

---
+++ learning-c/2-advanced/05-memory-management/exercises/2-dynamic-note-recording.c
@@ -0,0 +1,18 @@
+/*
+ * Dynamic Note Recording:
+ *
+ * Pseudocode:
+ *
+ * Reservar sizeof(int) de memoria. // 4byte
+ * Definir variable contador. // saber cuantas notas almacenamos
+ * Pedir al usuario que ingrese sus notas, indicarle
+ * que para terminar debe ingresar '-1'.
+ * Verificar el tamano de la memoria donde se ingresan
+ * notas, y si esta llena debemos  aumentar (*2) la memoria.
+ * Verificar que cuando ingrese '-1' el loop de
+ * pregunta debe terminar.
+ * Luego en una nueva variable float, calculamos el
+ * promedio. // se divide por la variable contador
+ * Mostrar promedio al usuario.
+ *
+ */
